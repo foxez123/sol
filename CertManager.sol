834753dbaa5eb0ca08da927f0c5a6184470579cd
foxez123 <30331829+foxez123@users.noreply.github.com>
2019-02-10 12:37:26 +0900

    Update CertManager.sol

--- CertManager.sol
+++ CertManager.sol
@@ -1,10 +1,8 @@
 pragma solidity 0.4.24;
 
 contract CertificateManager {
-    uint    certTypeIdGenerator; //default is 0
-    uint    certIdGenerator; //default is 0
-    //uint    idGenerator; //default is 0
-    
+    uint    certTypeIdGenerator = 1; //default is 1
+    uint    certIdGenerator = 1; //default is 1
     
     // uint: certTypeId
     mapping (uint => CertType) certTypes; 
@@ -13,12 +11,11 @@ contract CertificateManager {
         string name;
         string issuerName;
         string descUrl;
-        address issuerAddress;   // register address
-        bool isValid;     // 
-        // int grade; // for issuer grade later
+        address issuerAddress;      // register address
+        bool isValid;               // if problems is occurred, invalid the specific certType
     }
     
-    // uint personalCertId
+    // uint: certId
     mapping (uint => PersonalCert) personalCerts;
     struct PersonalCert {
         uint certId;
@@ -29,10 +26,28 @@ contract CertificateManager {
         uint certTypeId;
         bool isConfirmed;  // default false;
     }
-   
-    // The issuers register certificate
+    
+    address owner;
+    
+    modifier onlyOwner() {
+       require( owner == msg.sender );
+       _;
+        
+    }
+    
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+
+
+    /**
+    * The issuers register certificate
+    * sample: "TOEIC", "ETS", "https://www.ets.org/toeic"
+    * sample: "AWS Certification", "AWS", "https://aws.amazon.com/certification/?nc1=h_ls"
+    * sample: "Blockchain Proficiency", Consensys Academy", "https://consensys.net/academy/"
+    */ 
     function registerCertType(string _name, string _issuerName, string _descUrl) public returns(uint){
-        //require(msg.value == 1 ether);
         require( bytes(_name).length != 0);
         
         CertType memory newCertType;
@@ -49,22 +64,14 @@ contract CertificateManager {
         return certTypeIdGenerator-1;
     }
     
-    address owner;
-    
-    modifier onlyOwner() {
-       require( owner == msg.sender );
-       _;
-        
-    }
 
-    // condition: issuer only see his certType info and contract owner can see the all certType info
-    // return string: certTypeName
-    // return string: issuerName
-    // return string: descUrl
-    // sample data "TOEIC" "ETS" "https://www.ets.org/toeic"
-    // sample data "AWS Certification" "AWS" "https://aws.amazon.com/certification/?nc1=h_ls""
-    // sample data "Blockchain Proficiency" Consensys Academy" "https://consensys.net/academy/"
-    function getCertTypeInfo(uint _certTypeId) public view returns(string, string, string) {
+    /**
+    * The issuer can see his certType info and contract owner can see the all certType info
+    * return string: certTypeName
+    * return string: issuerName
+    * return string: descUrl
+    */
+    function viewCertTypeInfo(uint _certTypeId) public view returns(string, string, string) {
         require( certTypes[_certTypeId].issuerAddress == msg.sender || owner == msg.sender );
         
         return (
@@ -74,37 +81,41 @@ contract CertificateManager {
             );
     }
 
-       
-   
-   // The student regiester personal certificate
-   // TODO: add money
-   // TODO: send some money to issuer
-   function regiesterPersonalCert(string _issueDt, string _validThroughDt, string _certNo, uint _certTypeId) public returns(uint) {
-       require( bytes(certTypes[_certTypeId].name).length != 0 );
-       
-       PersonalCert memory newPersonalCert;
-       newPersonalCert.certId = certIdGenerator;
-       newPersonalCert.ownerAddress = msg.sender;
-       newPersonalCert.issuedDt = _issueDt;
-       newPersonalCert.validThroughDt = _validThroughDt;
-       newPersonalCert.certNo = _certNo;
-       newPersonalCert.certTypeId = _certTypeId;
-       newPersonalCert.isConfirmed = false;
-       
-       personalCerts[certIdGenerator] = newPersonalCert;
-       certIdGenerator++;
 
-       return certIdGenerator-1;
-   }
+    /** The student regiester personal certificate
+    * TODO: add money
+    * TODO: send some money to issuer
+    * sample data: "2018-01-01", "2022-01-01", "MyCert1", 1
+    * sample data: "2018-02-01", "2099-12-31", "MyCert2", 2
+    */
+    function regiesterPersonalCert(string _issueDt, string _validThroughDt, string _certNo, uint _certTypeId) public returns(uint) {
+        require( bytes(certTypes[_certTypeId].name).length != 0 ); // certType must be generated
+        require( bytes(_certNo).length != 0 ); // certNo should not be empty
+       
+        PersonalCert memory newPersonalCert;
+        newPersonalCert.certId = certIdGenerator;
+        newPersonalCert.ownerAddress = msg.sender;
+        newPersonalCert.issuedDt = _issueDt;
+        newPersonalCert.validThroughDt = _validThroughDt;
+        newPersonalCert.certNo = _certNo;
+        newPersonalCert.certTypeId = _certTypeId;
+        newPersonalCert.isConfirmed = false;
+       
+        personalCerts[certIdGenerator] = newPersonalCert;
+        certIdGenerator++;
+
+        return certIdGenerator-1;
+    }
    
    
-   
-    // The issuer confirm the certificate which was regisered by students
-    // Consider: why the issuer pay a gas?
-    // because of gas, 
+    /**
+    * The issuer confirm the certificate which was regisered by students
+    * Consider: why the issuer pay a gas?
+    * because of gas, 
+    */
     function confirmCert(uint _certId) public {
-        // check proper issuer
         require( personalCerts[_certId].certTypeId != 0 );  // check existence for personalCert
+        require( personalCerts[_certId].isConfirmed == false );  // for gas ..
         require( certTypes[ personalCerts[_certId].certTypeId ].issuerAddress == msg.sender );  // check certType issuer's address
         
         // update as confirmed
@@ -118,26 +129,33 @@ contract CertificateManager {
         uint certId;
         uint256 startDt;
         uint256 endDt;
+        uint validDay;
     }
     
     
-    // generate key value for personal certificate
-    function generateCertToken(uint _certId, uint _startDt, uint _endDt) public returns(address){
+    /**
+    * generate key value for personal certificate
+    * sample: 1, 10  (valid for 10 days)
+    * sample: 1, 0  (for invalid test)
+    */
+    function generateCertToken(uint _certId, uint _validDay) public returns(address){
         // check proper personalCert
-        require( personalCerts[_certId].ownerAddress != msg.sender ); // check owner (student)
+        require( personalCerts[_certId].ownerAddress == msg.sender ); // check owner (student)
         
         address tokenAddress = UniqueId.getUniqueId();
         
         CertToken memory newCertToken;
         newCertToken.certId = _certId;
-        newCertToken.startDt = _startDt;
-        newCertToken.endDt = _endDt;
+        newCertToken.startDt = now;
+        newCertToken.endDt = now + _validDay*60*60*24;
+        newCertToken.validDay = _validDay;
     
         certTokens[tokenAddress] = newCertToken;
         
         return tokenAddress;
     }
    
+    
     
     modifier checkTokenAddress(address _tokenAddress) {
        require( certTokens[_tokenAddress].tokenAddress == 0);
@@ -150,6 +168,7 @@ contract CertificateManager {
     // check token and certNo validation
     // return bool: true=valid, false=not valid
     // return string: reason
+    /*
     function checkCertToken(address _tokenAddress, string _certNo) checkTokenAddress(_tokenAddress) public view returns(bool) {
         
         if( keccak256(personalCerts[ certTokens[_tokenAddress].certId ].certNo) != keccak256(_certNo) ) {
@@ -158,18 +177,19 @@ contract CertificateManager {
             return true;
         }
     }
+    */
     
     
-    // in: ppcc(publishedPeronsalCertificates)
-    // return string: certTypeName
-    // return string: issuerName
-    // return string: descUrl (for certType)
-    
-    // return string: certNo
-    // return uint256: issuedDate
-    // return uint256: validThroughDt
-    
-    // TODO: add money
+    /**
+    * anyone who has a valid token can see the personal cert info
+    * return string: certTypeName
+    * return string: issuerName
+    * return string: descUrl (for certType)
+    * return string: certNo
+    * return uint256: issuedDate
+    * return uint256: validThroughDt
+    * TODO: add money
+    */
     function viewPersonalCert(address _tokenAddress) checkTokenAddress(_tokenAddress) public view returns(string, string, string, string, string, string) {
         uint certId = certTokens[_tokenAddress].certId;
         uint certTypeId = personalCerts[certId].certTypeId;
@@ -184,9 +204,19 @@ contract CertificateManager {
                
     }
     
-    function viewPersonalCert(uint _certId) public view returns(string, string, string, string, string, string) {
-        //uint certId = certTokens[_tokenAddress].certId;
-        //uint certTypeId = personalCerts[certId].certTypeId;
+    
+    /**
+    * student can see his personal cert info
+    * return string: certTypeName
+    * return string: issuerName
+    * return string: descUrl (for certType)
+    * return string: certNo
+    * return uint256: issuedDate
+    * return uint256: validThroughDt
+    * return bool: isConfirmed
+    */
+    function viewPersonalCert(uint _certId) public view returns(string, string, string, string, string, string, bool) {
+        require( personalCerts[_certId].ownerAddress == msg.sender || owner == msg.sender );
         
         return (
             certTypes[personalCerts[_certId].certTypeId].name,
@@ -194,8 +224,27 @@ contract CertificateManager {
             certTypes[personalCerts[_certId].certTypeId].descUrl,
             personalCerts[_certId].certNo, 
             personalCerts[_certId].issuedDt,
-            personalCerts[_certId].validThroughDt);
+            personalCerts[_certId].validThroughDt,
+            personalCerts[_certId].isConfirmed);
                
+    }
+    
+    
+    /**
+    * anybody can see the token info because it doesn't include any personal data
+    * return uint: certId
+    * return uint: startDt
+    * return uint: durationDay 
+    * To-be consideration: finding specific student's token is not easy.. 
+    *                      need to change the structure of this code.. 
+    *                      need to manage both all token list and only specific student's tokens ..
+    */
+    function viewTokenDetail(address _tokenAddress) public view returns(uint, uint, uint) {
+        return (
+            certTokens[_tokenAddress].certId,
+            certTokens[_tokenAddress].startDt,
+            certTokens[_tokenAddress].validDay
+            );
     }
 }
 
